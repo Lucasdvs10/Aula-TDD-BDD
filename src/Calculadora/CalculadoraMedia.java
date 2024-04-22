a46cb8c09e9ddaf9a4589c197308962e417ac9b7
Lucas Duez <46378322+Lucasdvs10@users.noreply.github.com>
2024-04-22 09:11:17 -0300

    Criar classe CalcularMedia e sua classe de teste
    - A classe CalcularMediaDeve corresponde aos testes da classe CalcularMedia
    
    - Criou-se o teste para a funcao Somar
    - Criou-se a funcao somar

---
+++ src/Calculadora/CalculadoraMedia.java
@@ -0,0 +1,8 @@
+package Calculadora;
+
+public class CalculadoraMedia {
+    public int Soma(int a, int b){
+        return a+b;
+    }
+
+}
---
+++ src/Testes/CalculadoraMediaDeve.java
@@ -0,0 +1,19 @@
+package Testes;
+
+import Calculadora.CalculadoraMedia;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class CalculadoraMediaDeve {
+    CalculadoraMedia calculadoraMedia;
+    @BeforeEach
+     void SetupTests(){
+        calculadoraMedia = new CalculadoraMedia();
+    }
+
+    @Test
+    public void Retornar4QuandoSomar2E2(){
+        Assertions.assertEquals(4, calculadoraMedia.Soma(2,2));
+    }
+}
