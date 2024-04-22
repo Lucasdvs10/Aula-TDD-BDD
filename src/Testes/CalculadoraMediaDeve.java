26d8c34987cc84c2c0210f10945ca932a0272f72
Lucas Duez <46378322+Lucasdvs10@users.noreply.github.com>
2024-04-22 09:20:43 -0300

    Escrever teste de aceitacao da funcao somar

--- src/Testes/CalculadoraMediaDeve.java
+++ src/Testes/CalculadoraMediaDeve.java
@@ -12,10 +12,27 @@ public class CalculadoraMediaDeve {
         calculadoraMedia = new CalculadoraMedia();
     }
 
+    /*
+        Teste a ser executado:
+        Funcionalidade: Somar dois numeros
+
+        Eu como usuário
+        quero efetuar a Soma de dois numeros inteiros
+        para facilitar meus calculos
+
+        Caso de teste:
+
+        Cenario: Somar dois numeros inteiros
+        Dado: ao selecionar a opcao de somar
+        Quando: ao digitar dois inteiros no terminal
+        Entao: deve exibir a soma dos dois numeros digitados
+     */
     @Test
     public void Retornar4QuandoSomar2E2(){
         Assertions.assertEquals(4, calculadoraMedia.Soma(2,2));
     }
+
+
 
     @Test
     public void Retornar2QuandoCalcularAMediaDe2E2(){
