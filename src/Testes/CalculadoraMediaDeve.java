f6a4278ef20f8184a167a8927afd33f0675551ef
Lucas Duez <46378322+Lucasdvs10@users.noreply.github.com>
2024-04-22 09:20:56 -0300

    Escrever teste de aceitacao da funcao Media

--- src/Testes/CalculadoraMediaDeve.java
+++ src/Testes/CalculadoraMediaDeve.java
@@ -33,6 +33,21 @@ public class CalculadoraMediaDeve {
     }
 
 
+    /*
+        Teste a ser executado:
+        Funcionalidade: Calcular a media de dois inteiros
+
+        Eu como usuário
+        quero cacular a Media de dois numeros inteiros
+        para ver se meus alunos foram aprovados
+
+        Caso de teste:
+
+        Cenario: Calcular media de dois numeros inteiros
+        Dado: ao selecionar a opcao de cacular media
+        Quando: ao digitar dois inteiros no terminal
+        Entao: deve exibir a media aritimetica dos dois numeros digitados
+     */
 
     @Test
     public void Retornar2QuandoCalcularAMediaDe2E2(){
